6b0b569318289e0b40a02d2ed0b51d6c0f17872e
Marcius Vinicius Esquecula <103079478+MarciusAMS@users.noreply.github.com>
2025-03-20 14:37:08 -0300

    Ajustes função

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,20 @@
-const varNome = document.getElementById('nome');
+const varEmail = document.getElementById('email');
 const varSenha = document.getElementById('senha');
 const varEnviar = document.getElementById('enviar');
 
 const login = {
-    nome: 'Pedro',
+    nome: 'pedro@gmail.com',
     senha: '1234',
 }
 
-function validarCampos(varNome, varSenha) {
-    if (varNome == '' && varSenha == '') {
+function validarCampos(varEmail, varSenha) {
+
+    if (varEmail == '' && varSenha == '') {
         window.alert('Todos os campos são obrigatórios!!!');
-    }
-    if (varNome != login.nome && varSenha != login.senha) {
-        window.alert('Nome ou senha incorretos!');
-    } else {
+    } else if (varEmail === login.email && varSenha === login.senha) {
         window.alert('Login realizado com sucesso!!!');
+    } else {
+        window.alert('Email ou senha incorretos!');
     }
+
 }
