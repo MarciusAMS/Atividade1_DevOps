b5b642734cb6b93cb684018f73ee5426c8448524
Marcius Vinicius Esquecula <lauramoreira887@gmail.com>
2025-03-20 14:46:04 -0300

    Ajustes na verificação

--- JS/index.js
+++ JS/index.js
@@ -3,15 +3,13 @@ const varSenha = document.getElementById('senha');
 const varEnviar = document.getElementById('enviar');
 
 const login = {
-    nome: 'pedro@gmail.com',
+    email: 'pedro@gmail.com',
     senha: '1234',
 }
 
 function validarCampos(varEmail, varSenha) {
 
-    if (varEmail == '' && varSenha == '') {
-        window.alert('Todos os campos são obrigatórios!!!');
-    } else if (varEmail === login.email && varSenha === login.senha) {
+    if (varEmail === login.email && varSenha === login.senha) {
         window.alert('Login realizado com sucesso!!!');
     } else {
         window.alert('Email ou senha incorretos!');
